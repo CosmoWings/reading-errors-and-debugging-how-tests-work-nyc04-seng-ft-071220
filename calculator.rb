97a09954c239092b60d1777656c99cec998f9fbb
Patrick Pierre <patrick.pierre000@gmail.com>
2020-07-15 14:35:19 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,7 +2,7 @@
 first_number = 2 
 second_number = 2 
 
-sum = first_numer + second_number
+sum = first_number + second_number
 difference = first_numer - second_number
 product = first_numer * second_number
 quotient = first_numer / second_number
