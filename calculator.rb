25e40ff9f556100554d1713a243e391d2e0f6cba
Patrick Pierre <patrick.pierre000@gmail.com>
2020-07-15 14:35:00 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,6 +3,6 @@ first_number = 2
 second_number = 2 
 
 sum = first_numer + second_number
-sum = first_numer + second_number
-sum = first_numer + second_number
-sum = first_numer + second_number
+difference = first_numer - second_number
+product = first_numer * second_number
+quotient = first_numer / second_number
